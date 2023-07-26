1b0c3bb3e4ad6a4f0f7596aafc1d0bbdcc52f175
Kamlesh Kumar <99287810+kamleshsaini1011@users.noreply.github.com>
2023-07-26 11:27:17 +0530

    Update ML.py

--- ML.py
+++ ML.py
@@ -5,7 +5,7 @@ from langchain.llms.openai import OpenAI
 from langchain.agents.agent_types import AgentType
 from langchain.chat_models import ChatOpenAI 
 agent_executor = create_python_agent(
-    llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613" , openai_api_key="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"),
+    llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613" , openai_api_key="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"),
     tool=PythonREPLTool(),
     verbose=True,
     agent_type=AgentType.OPENAI_FUNCTIONS,
